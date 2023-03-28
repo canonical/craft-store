f2249543714b66518e396b5c30ea2ef46a63b9ad
Alex Lowe <alex.lowe@canonical.com>
2023-03-28 16:21:57 -0400

    models: store models for registered name

---
+++ tests/unit/models/test_registered_name_model.py
@@ -0,0 +1,147 @@
+#  -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*
+#
+#  Copyright 2023 Canonical Ltd.
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License version 3 as published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""Tests for RegisteredNameModel."""
+import re
+from datetime import datetime, timezone
+
+import pytest
+
+from craft_store.models import RegisteredNameModel, AccountModel, TrackGuardrailModel, TrackModel
+from craft_store.models.registered_name_model import MediaModel
+
+BASIC_REGISTERED_NAME = {
+    "id": "123",
+    "private": "true",
+    "publisher": {"id": "456"},
+    "status": "registered",
+    "store": "ubuntu",
+    "type": "charm",
+}
+REGISTERED_NAME_ALL_FIELDS = {
+    "authority": "Mark",
+    "contact": "charmcrafters@lists.canonical.com",
+    "default-track": "lts",
+    "description": "This is a thing",
+    "id": "123",
+    "links": ["https://ubuntu.com/"],
+    "media": [{"type": "icon", "url": "https://assets.ubuntu.com/v1/0843d517-favicon.ico"}],
+    "name": "charming-charm",
+    "private": "false",
+    "publisher": {"id": "456"},
+    "status": "registered",
+    "store": "ubuntu",
+    "summary": "This is a thing",
+    "title": "Some charming charm",
+    "track-guardrails": [{
+        "created-at": "2023-03-28T18:50:44+00:00",
+        "pattern": r"^\d\.\d/"
+    }],
+    "tracks": [{
+        "created-at": "2023-03-28T18:50:44+00:00",
+        "name": "1.0/stable"
+    }],
+    "type": "charm",
+    "website": "https://canonical.com"
+}
+
+
+@pytest.mark.parametrize(
+    "json_dict,expected",
+    [
+        pytest.param(
+            BASIC_REGISTERED_NAME,
+            RegisteredNameModel(
+                id="123",
+                private=True,
+                publisher=AccountModel(id="456"),
+                status="registered",
+                store="ubuntu",
+                media=[],
+                tracks=[],
+                type="charm",
+                **{"track-guardrails": []}
+            ),
+            id="basic"
+        ),
+        pytest.param(
+            REGISTERED_NAME_ALL_FIELDS,
+            RegisteredNameModel(
+                authority='Mark',
+                contact='charmcrafters@lists.canonical.com',
+                description='This is a thing',
+                id='123',
+                links=['https://ubuntu.com/'],
+                media=[
+                    MediaModel(
+                        type='icon',
+                        url='https://assets.ubuntu.com/v1/0843d517-favicon.ico',
+                    )
+                ],
+                name='charming-charm',
+                private=False,
+                publisher=AccountModel(id='456'),
+                status='registered',
+                store='ubuntu',
+                summary='This is a thing',
+                title='Some charming charm',
+                tracks=[
+                    TrackModel.unmarshal({
+                        "created-at": "2023-03-28T18:50:44+00:00",
+                        "name": "1.0/stable"
+                    }),
+                ],
+                type='charm',
+                website='https://canonical.com',
+                **{
+                    "default-track": "lts",
+                    "track-guardrails": [
+                        TrackGuardrailModel.unmarshal({
+                            "created-at": "2023-03-28T18:50:44+00:00",
+                            "pattern": r"^\d\.\d/"
+                        })
+                    ],
+                }
+            ),
+            id="all_fields"
+        )
+    ]
+)
+def test_unmarshal(json_dict, expected):
+    actual = RegisteredNameModel.unmarshal(json_dict)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize("payload", [BASIC_REGISTERED_NAME, REGISTERED_NAME_ALL_FIELDS])
+def test_unmarshal_and_marshal(payload, check):
+    marshalled = RegisteredNameModel.unmarshal(payload).marshal()
+    not_set = [[["NOT SET"]]]
+
+    check.equal(marshalled.keys(), payload.keys())
+
+    for field in payload:
+        actual = marshalled.get(field, not_set)
+        expected = payload.get(field, not_set)
+        if field == "private":
+            expected = payload[field].lower() == "true"
+        elif field in ("track-guardrails", "tracks"):
+            expected = payload[field].copy()
+            for item in expected:
+                item["created-at"] = datetime.fromisoformat(item["created-at"])
+                if field == "track-guardrails":
+                    item["pattern"] = re.compile(item["pattern"])
+        check.equal(actual, expected)
---
+++ tests/unit/models/test_track_guardrail_model.py
@@ -0,0 +1,72 @@
+#  -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*
+#
+#  Copyright 2023 Canonical Ltd.
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License version 3 as published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""Tests for TrackGuardrailModel"""
+import re
+
+#  -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+#  Copyright 2023 Canonical Ltd.
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License version 3 as published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""Tests for the track guardrail model."""
+from datetime import datetime, timezone
+
+import pytest
+
+from craft_store.models.track_guardrail_model import TrackGuardrailModel
+
+GUARDRAIL_DICT = {
+    "created-at": "2023-03-28T18:50:44+00:00",
+    "pattern": r"^\d\.\d/"
+}
+
+
+@pytest.mark.parametrize(
+    "json_dict,expected",
+    [
+        pytest.param(
+            GUARDRAIL_DICT,
+            TrackGuardrailModel(
+                pattern=re.compile(r"^\d\.\d/"),
+                **{"created-at": datetime(2023, 3, 28, 18, 50, 44, tzinfo=timezone.utc),}
+            ),
+        ),
+    ]
+)
+def test_unmarshal(json_dict, expected):
+    actual = TrackGuardrailModel.unmarshal(json_dict)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize("payload", [GUARDRAIL_DICT])
+def test_unmarshal_and_marshal(payload, check):
+    marshalled = TrackGuardrailModel.unmarshal(payload).marshal()
+
+    check.equal(payload["pattern"], marshalled["pattern"].pattern)
+    check.equal(payload["created-at"], marshalled["created-at"].isoformat())
