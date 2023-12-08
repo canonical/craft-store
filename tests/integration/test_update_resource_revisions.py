0533f249d8775212c7804a923a85675ddd36a1f2
Alex Lowe <alex.lowe@canonical.com>
2023-12-07 20:13:46 -0500

    fix: multiple updates in test_update_resource_revisions

--- tests/integration/test_update_resource_revisions.py
+++ tests/integration/test_update_resource_revisions.py
@@ -28,32 +28,41 @@ def test_charm_update_resource_revisions(charm_client, charmhub_charm_name):
     resource_name = "empty-file"
 
     revisions = charm_client.list_resource_revisions(charmhub_charm_name, resource_name)
-    revision_number = revisions[-1].revision
+    revision_numbers = [r.revision for r in revisions[-2:]]
+    assert len(revision_numbers) == 2, "Needs more resource revisions"
 
-    resource_update = CharmResourceRevisionUpdateRequest(
-        revision=revision_number,
-        bases=[
-            RequestCharmResourceBase(
-                name="ubuntu",
-                channel="22.04",
-                architectures=["riscv64"],
-            )
-        ],
-    )
-    default_update = CharmResourceRevisionUpdateRequest(
-        revision=revision_number,
-        bases=[
-            RequestCharmResourceBase(name="all", channel="all", architectures=["all"])
-        ],
-    )
+    resource_updates = [
+        CharmResourceRevisionUpdateRequest(
+            revision=revision_number,
+            bases=[
+                RequestCharmResourceBase(
+                    name="ubuntu",
+                    channel="22.04",
+                    architectures=["riscv64"],
+                )
+            ],
+        )
+        for revision_number in revision_numbers
+    ]
+    default_updates = [
+        CharmResourceRevisionUpdateRequest(
+            revision=revision_number,
+            bases=[
+                RequestCharmResourceBase(
+                    name="all", channel="all", architectures=["all"]
+                )
+            ],
+        )
+        for revision_number in revision_numbers
+    ]
 
     update_count = charm_client.update_resource_revisions(
-        resource_update, name=charmhub_charm_name, resource_name="empty-file"
+        *resource_updates, name=charmhub_charm_name, resource_name="empty-file"
     )
-    assert update_count == 1
+    assert update_count == len(resource_updates)
 
     # Reset back to the default.
     update_count = charm_client.update_resource_revisions(
-        default_update, name=charmhub_charm_name, resource_name="empty-file"
+        *default_updates, name=charmhub_charm_name, resource_name="empty-file"
     )
-    assert update_count == 1
+    assert update_count == len(default_updates)
