1aff0b2955d667c9042959c4dd733c4e1666d347
Alex Lowe <alex.lowe@canonical.com>
2024-05-22 16:36:11 -0400

    style: manual ruff lint fixes

--- craft_store/auth.py
+++ craft_store/auth.py
@@ -265,7 +265,7 @@ class Auth:
             encoded_credentials_string = self._keyring.get_password(
                 self.application_name, self.host
             )
-        except Exception as unknown_error:
+        except Exception as unknown_error:  # noqa: BLE001
             logger.debug(
                 "Unhandled exception raised when retrieving credentials: %r",
                 unknown_error,
